8481c6715399b87f7b508ad4d2f5a19a766905a9
Dan Yalg <dan@thecodingforge.com.au>
2023-07-19 21:28:49 +1000

    adding update hot reloads

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
+import * as Updates from 'expo-updates';
 
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
+Updates.reloadAsync();
 registerRootComponent(App);

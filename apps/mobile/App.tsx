65ff3aeb5e06918fcb924b32ae131e14ab76bf69
Dan Yalg <dan@thecodingforge.com.au>
2023-07-20 09:57:19 +1000

    tweaking CI

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeIcon style={{ fontSize: 60 }} />
+      <HomeIcon style={{ fontSize: 65 }} />
       <HomeScreen />
     </View>
   );

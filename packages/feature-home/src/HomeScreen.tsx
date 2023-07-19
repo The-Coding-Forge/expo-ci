c52fc7d6498c872bea6098940c1573e5ccb5acb0
Dan Yalg <dan@thecodingforge.com.au>
2023-07-19 22:32:29 +1000

    adding env var

--- packages/feature-home/src/HomeScreen.tsx
+++ packages/feature-home/src/HomeScreen.tsx
@@ -3,4 +3,6 @@ import { ComponentProps } from 'react';
 
 type HomeScreenProps = ComponentProps<typeof Paragraph>;
 
-export const HomeScreen = (props: HomeScreenProps) => <Paragraph {...props}>444</Paragraph>;
+export const HomeScreen = (props: HomeScreenProps) => (
+  <Paragraph {...props}>{process.env.EXPO_PUBLIC_NUMBER}</Paragraph>
+);

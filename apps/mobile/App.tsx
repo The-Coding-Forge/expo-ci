a8a19b05e29df6eaf58a4a68dc434663ebabb256
Dan Yalg <dan@thecodingforge.com.au>
2023-07-20 10:18:36 +1000

    changing app code

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeIcon style={{ fontSize: 65 }} />
+      <HomeIcon style={{ fontSize: 55 }} />
       <HomeScreen />
     </View>
   );
